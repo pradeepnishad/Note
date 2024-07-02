0052e8911e2a51ae9a817e5da4f2affb49d02c6a
devdeep <88630284+pradeepnishad@users.noreply.github.com>
2024-07-02 15:41:27 +0530

    first commit

---
+++ website/static/index.js
@@ -0,0 +1,10 @@
+// console.log("hello")
+
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
